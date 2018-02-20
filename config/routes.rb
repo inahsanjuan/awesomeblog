1822692ca9f22007f5651947d80e2346dfa12881
Inah San Juan <inahsanjuan@gmail.com>
2018-02-20 21:45:22 +0800

    Gravatar and sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/show'
+
   get 'user/new'
 
   root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
 
+  resources :users
   # For details on the DSL availaable within this file, see http://guides.rubyonrails.org/routing.html
 end
