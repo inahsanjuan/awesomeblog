8d47c9feb53ec4c990484890fb4a0f19668eacaf
Inah San Juan <inahsanjuan@gmail.com>
2018-02-22 22:06:42 +0800

    Show users in index user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user,  only: [:index, :edit, :update]
+  before_action :correct_user,    only: [:edit, :update]
+
   def index
     @users = User.all
   end
@@ -27,5 +30,13 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to log_path
+      end
     end
 end
