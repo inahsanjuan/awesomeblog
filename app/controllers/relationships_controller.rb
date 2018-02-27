a2a6c9309048e329843ec9f2f8239f9460e3311f
Inah San Juan <inahsanjuan@gmail.com>
2018-02-27 12:02:10 +0800

    Show all feeds from followed user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,11 +5,11 @@ class RelationshipsController < ApplicationController
     user = User.find(params[:followed_id])
     current_user.follow(user)
     redirect_to user
-    end
+  end
 
-    def destroy
+  def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
     redirect_to user
-    end
+  end
 end
